63a9d66ab18b95634ad9bda25dc5d52ce58aabf6
Andreas Nedbal <andy@pixelde.su>
2021-01-01 21:29:53 +0100

    :sparkles: properly show duration as mm:ss instead just seconds

--- src/renderer/components/TrackTableEntry.vue
+++ src/renderer/components/TrackTableEntry.vue
@@ -3,7 +3,7 @@
     <td class="border-b border-gray-300">{{ track.track_num }}</td>
     <td class="border-b border-gray-300">{{ track.title }}</td>
     <td class="border-b border-gray-300"><audio :src="track.file['mp3-128']" controls></audio></td>
-    <td class="border-b border-gray-300">{{ track.duration }}</td>
+    <td class="border-b border-gray-300">{{ duration }}</td>
   </tr>
 </template>
 
@@ -12,6 +12,15 @@ export default {
   name: 'TrackTableEntry',
   props: {
     track: Object
+  },
+  computed: {
+    duration: function () {
+      const roundDuration = Math.floor(this.track.duration)
+      let minutes = Math.floor(roundDuration / 60)
+      let seconds = (roundDuration - minutes * 60).toString().padStart(2, '0')
+
+      return `${minutes}:${seconds}`
+    }
   }
 }
 </script>
