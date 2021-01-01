cdde01e3b9c1b25c84f793022e88da1a70421268
Andreas Nedbal <andy@pixelde.su>
2021-01-01 23:36:26 +0100

    :recycle: move contents from App into Album page

---
+++ src/renderer/pages/Album.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="TralbumData">
+    <div class="flex p-4">
+      <img class="max-w-xs h-auto" :src="'https://f4.bcbits.com/img/a' +  TralbumData.art_id  + '_16.jpg'"/>
+      <div class="flex-grow self-center ml-4">
+        <h1 class="font-extrabold text-6xl">{{ TralbumData.current.title }}</h1>
+        <p class="font-bold mb-2">{{ TralbumData.current.artist || TralbumData.artist }}</p>
+        <p>{{TralbumData.trackinfo.length }} tracks, released {{ releaseDate }}</p>
+      </div>
+    </div>
+  
+    <div class="p-4">
+      <track-table :trackinfo="TralbumData.trackinfo"/>
+    </div>
+  </div>
+  <webview src="https://frozenstarfall.bandcamp.com/album/infinite-dreams" preload="preload.js" nodeintegration style="display: none;"></webview>
+</template>
+
+<script>
+import TrackTable from '../components/TrackTable.vue'
+
+export default {
+  name: 'Album',
+  components: {
+    TrackTable
+  },
+  data() {
+    return {
+      TralbumData: null
+    }
+  },
+  computed: {
+    releaseDate: function () {
+      let date = new Date(this.TralbumData.current.release_date)
+
+      return date.toLocaleDateString()
+    }
+  },
+  mounted() {
+    let webview = document.querySelector('webview')
+
+    webview.addEventListener('ipc-message', event => {
+      this.TralbumData = event.args[0]
+    })
+  }
+}
+</script>
