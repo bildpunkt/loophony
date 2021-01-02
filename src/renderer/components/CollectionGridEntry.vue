95e75d595a95e41aab95d22252fd95b2bd0ae472
Andreas Nedbal <andy@pixelde.su>
2021-01-02 17:52:36 +0100

    :sparkles: link collection items to album pages

--- src/renderer/components/CollectionGridEntry.vue
+++ src/renderer/components/CollectionGridEntry.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex-1 m-4">
-    <img class="max-w-xs h-auto" :src="item.item_art.url"/>
-    <p class="font-bold">{{ item.album_title }}</p>
+    <router-link :to="'/album?url=' + item.item_url">
+      <img class="max-w-xs h-auto" :src="item.item_art.url"/>
+      <p class="font-bold">{{ item.album_title }}</p>
+    </router-link>
     <p>{{ item.band_name }}</p>
   </div>
 </template>
