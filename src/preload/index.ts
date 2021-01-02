fc094bdb6f38ab4e77ce37c9474802a1640ddab6
Andreas Nedbal <andy@pixelde.su>
2021-01-02 03:03:33 +0100

    :sparkles: initial display of profile data

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const { ipcRenderer } = require('electron')
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,5 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (window['TralbumData'] !== undefined) {
     ipcRenderer.sendToHost('data', window['TralbumData'])
+  }
+
+  if (window['CollectorsAPI'] !== undefined && window['FanData'] !== undefined) {
+    const fanID = FanData.fan_id
+    const lastToken = document.querySelector('[data-token]').getAttribute('data-token')
+
+    CollectorsAPI
+      .doPost('/api/fancollection/1/collection_items', 
+      `{"fan_id":${fanID},"older_than_token":"${lastToken}","count":9999}`).then(function (data) {
+        ipcRenderer.sendToHost('data', {
+          FanData: window.FanData,
+          CollectionItems: data.items
+        })
+      })
   }
 })
--- src/renderer/pages/Profile.vue
+++ src/renderer/pages/Profile.vue
@@ -1,9 +1,46 @@
 <template>
-  Welcome to loophony, {{ $route.params.user }}
+  <div class="flex p-4" v-if="FanData">
+    <img class="max-w-xs h-auto" :src="photoURL"/>
+    <div class="flex-grow self-center ml-4">
+      <h1 class="font-extrabold text-6xl mb-4">{{ FanData.username }}</h1>
+      <p>{{ FanData.followers_count }} followers, {{ followingCount }} following</p>
+    </div>
+  </div>
+  <div class="flex flex-wrap p-4" v-if="CollectionItems">
+    <div class="w-1/5 pb-4" v-for="item in CollectionItems" :key="item.album_id">
+      <img class="max-w-xs h-auto" :src="item.item_art.url"/>
+      <p class="font-bold">{{ item.album_title }}</p>
+      <p>{{ item.band_name }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Profile'
+  name: 'Profile',
+  data() {
+    return {
+      FanData: null,
+      CollectionItems: null
+    }
+  },
+  computed: {
+    photoURL: function () {
+      return `https://f4.bcbits.com/img/${this.FanData.photo.image_id.toString().padStart(10, '0')}_41.jpg`
+    },
+    followingCount: function () {
+      return Number(this.FanData.following_fans_count) + Number(this.FanData.following_bands_count)
+    }
+  },
+  mounted() {
+    let webview = document.querySelector('[bandcamp-navigator]')
+
+    webview.src = `https://bandcamp.com/${this.$route.params.user}`
+
+    webview.addEventListener('ipc-message', event => {
+      this.FanData = event.args[0].FanData
+      this.CollectionItems = event.args[0].CollectionItems
+    })
+  }
 }
 </script>
